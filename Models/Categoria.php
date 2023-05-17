4301382d6e812163d47ceaa4882e03c80f08c979
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-17 17:31:17 +0200

    addded class Categoria

---
+++ Models/Categoria.php
@@ -0,0 +1,20 @@
+<?php
+
+class Categoria 
+{
+    private $nome, $icona;
+
+    public function __construct($nome, $icona)
+    {
+        $this->nome = $nome;
+        $this->icona = $icona;
+    }
+    
+    public function getNome(){
+        return $this->nome;
+    }
+    public function getIcona(){
+        return $this->icona;
+    }
+        
+}
