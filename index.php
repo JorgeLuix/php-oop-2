8e20af914f23fd53d7d1bab5f2a449d6c22d00c1
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-17 18:47:19 +0200

    added style layout

--- index.php
+++ index.php
@@ -10,13 +10,18 @@ include __DIR__.'./data/db.php'
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Shop</title>
 </head>
 <body>
-    <div class="container">
+    <header>
+    <?php include './Views/Header.php';?>
+    </header>
+    <main>
+    <div class="container p-5">
         <div class="row">
         
-    <?php
+        <?php
         function stampaCardProdotto($prodotto){
             
             echo '<div class="col-md-3 mb-3">';
@@ -39,5 +44,9 @@ include __DIR__.'./data/db.php'
      ?>
      </div>
     </div>
+    </main>
+    <footer>
+    <?php include './Views/Footer.php';?>
+    </footer>
 </body>
 </html>
