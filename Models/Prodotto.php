7384416803dd771fc659c09e8d23a485582875a4
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-18 15:53:26 +0200

    added trai on prodotto.php

--- Models/Prodotto.php
+++ Models/Prodotto.php
@@ -1,13 +1,32 @@
 <?php
 
+trait CategoriaTrait {
+    private $categoria;
+
+    public function setCategoria($categoria){
+        if(!$categoria instanceof Categoria) {
+            throw new Exception('La categoria deve essere un\'instanza della classe Categoria.');
+        }
+
+        $this->categoria = $categoria;
+
+    }
+
+    public function getCategoria(){
+        return $this->categoria;
+    }
+}
+
 class Prodotto 
 {
+    use CategoriaTrait;
+
     private $id, $categoria, $tipo, $titolo, $prezzo, $immagine;
 
     public function __construct($id, $categoria, $tipo, $titolo, $prezzo, $immagine)
     {
         $this->id = $id;
-        $this->categoria = $categoria;
+        $this->setCategoria($categoria);
         $this->tipo = $tipo;
         $this->titolo = $titolo;
         $this->prezzo = $prezzo;
--- data/db.php
+++ data/db.php
@@ -3,7 +3,7 @@ include './Models/Prodotto.php';
 include './Models/Categoria.php';
 
 // Creazione delle categorie
-$categoriaCani = new Categoria("Cani", "./img/cane.jpg");
+$categoriaCani = new Categoria("Cani", "fa-solid fa-dog");
 $categoriaGatti = new Categoria("Gatti", "./img/gatto.webp");
 
 // Creazione dei prodotti
@@ -11,3 +11,7 @@ $prodotto1 = new Prodotto(1, $categoriaCani, "Prodotto", "Giocattolo per cani", 
 $prodotto2 = new Prodotto(2, $categoriaCani, "Cibo", "Crocchette per cani", 19.99, "./img/crocchette.webp");
 $prodotto3 = new Prodotto(3, $categoriaGatti, "Prodotto", "Giocattolo per gatti", 8.99, "./img/bubba.jpg");
 $prodotto4 = new Prodotto(4, $categoriaGatti, "Cibo", "Croccantini per gatti", 15.99, "./img/cibogatti.jpg");
+
+
+    
+?>
